dfe4bd6991a5cf6379aad7cef67077ef9b51dd82
lab4_6 <lab4_6@DESKTOP-QL6G9F2>
2017-10-04 19:39:02 -0300

    Add .gitignore and .gitattributes.

---
+++ Atividade04-10/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Atividade04_10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exibir(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show("Data selecionada é :\n"
+                + "Dia: " + dtpData.SelectedDate.Value.Day.ToString()
+                + "\nMes: " + dtpData.SelectedDate.Value.Month.ToString()
+                + "\nAno: " + dtpData.SelectedDate.Value.Year.ToString(),
+                "Resultado");
+            
+        }
+    }
+}
